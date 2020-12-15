27107cbf4922a9be8301288d28a3e9d9723469b4
James Lafferty <james@j-lafferty.net>
2020-12-14 18:07:33 -0800

    Update admin and add rango app directories

--- tango_with_django/urls.py
+++ tango_with_django/urls.py
@@ -1,4 +1,4 @@
-"""rango URL Configuration
+"""tango_with_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
