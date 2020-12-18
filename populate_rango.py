01fabe2b1fa9e9ba2cf65db25e91bae80c44045b
James Lafferty <james@j-lafferty.net>
2020-12-18 08:37:46 -0800

    Update with end of chapter5 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -24,20 +24,20 @@ def populate():
     # and add the data to our models.
 
     python_pages = [
-        {'title': 'Offical Python Tutorial',
+        {'title': 'Official Python Tutorial',
          'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How To Think Like A Computer Scientist',
+        {'title': 'How to Think like a Computer Scientist',
          'url': 'http://www.greenteapress.com/thinkpython/'},
         {'title': 'Learn Python in 10 Minutes',
          'url': 'http://www.korokithakis.net/tutorials/python/'},
     ]
 
     django_pages = [
-        {'title': 'Offical Django Tutorial',
+        {'title': 'Official Django Tutorial',
          'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial101/'},
         {'title': 'Django Rocks',
          'url': 'http://www.djangorocks.com/'},
-        {'title': 'How To Tango With Django',
+        {'title': 'How to Tango with Django',
          'url': 'http://www.tangowithdjango.com/'},
     ]
 
--- rango/admin.py
+++ rango/admin.py
@@ -4,6 +4,7 @@ from rango.models import Category, Page
 # Register your models here.
 
 class PageAdmin(admin.ModelAdmin):
+    fields = ('title', 'category', 'url')
     list_display = ('title', 'category', 'url')
 
 admin.site.register(Category)
