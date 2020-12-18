f3be958d6f5e3afc5c6cd39bd8a54a73aa35a734
James Lafferty <james@j-lafferty.net>
2020-12-18 12:46:38 -0800

    Update admin to auto insert category slug url

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# Register your models here.
-
 class PageAdmin(admin.ModelAdmin):
     fields = ('title', 'category', 'url')
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
